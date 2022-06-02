809ea67aa04fd4e628290d25125df41afdab7ff1
Gijs Hendriksen <info@gijshendriksen.nl>
2022-06-02 12:37:33 +0200

    Add script for computing metrics and analyzing failures

--- analysis/tables.py
+++ analysis/tables.py
@@ -8,12 +8,12 @@ import pandas as pd
 import wandb
 
 
-def get_wandb_tables(tables_root: Path, sweep_id: str) -> Dict[str, pd.DataFrame]:
+def get_wandb_tables(tables_root: Path, sweep_id: str, table_type: str = 'instances') -> Dict[str, pd.DataFrame]:
     tables_dir = tables_root / sweep_id
 
     if os.path.exists(tables_dir):
         return {
-            filename[:-4]: pd.read_csv(tables_dir / filename)
+            filename[:-4]: pd.read_csv(tables_dir / filename, dtype=str).fillna('')
             for filename in os.listdir(tables_dir)
             if filename.endswith('.csv')
         }
@@ -27,7 +27,7 @@ def get_wandb_tables(tables_root: Path, sweep_id: str) -> Dict[str, pd.DataFrame
 
     for run in sweep.runs:
         for file in run.files():
-            if file.name.startswith('media/table/instances/test_'):
+            if file.name.startswith(f'media/table/{table_type}/test_'):
                 print(f'Downloading {run.name}: {file.name}')
                 with file.download(root=str(tables_dir), replace=True) as _file:
                     table_data = json.load(_file)
