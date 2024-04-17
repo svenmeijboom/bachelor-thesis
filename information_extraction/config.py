035a913263be63009b97e0d459cb8282df24659e
svenmeijboom <svenmeijboom@gmail.com>
2024-04-17 12:49:25 +0200

    Changed DATA_DIR

--- information_extraction/config.py
+++ information_extraction/config.py
@@ -1,4 +1,5 @@
 from pathlib import Path
 
 WANDB_PROJECT = 'information_extraction'
-DATA_DIR = Path('~/Data/').expanduser()
+#DATA_DIR = Path('~/Data/').expanduser()
+DATA_DIR = "/vol/csedu-nobackup/other/smeijboom/Data"
