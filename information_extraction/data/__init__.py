02ef58132f5d7d54750c700ea40b25c17bc6ca35
svenmeijboom <svenmeijboom@gmail.com>
2024-05-01 11:12:43 +0200

    changed domains

--- information_extraction/data/__init__.py
+++ information_extraction/data/__init__.py
@@ -1,10 +1,14 @@
 DOMAINS = {
-    'auto': ['engine', 'fuel_economy', 'model', 'price'],
-    'book': ['author', 'isbn_13', 'publication_date', 'publisher', 'title'],
-    'camera': ['manufacturer', 'model', 'price'],
-    'job': ['company', 'date_posted', 'location', 'title'],
-    'movie': ['director', 'genre', 'mpaa_rating', 'title'],
-    'nbaplayer': ['height', 'name', 'team', 'weight'],
-    'restaurant': ['address', 'cuisine', 'name', 'phone'],
-    'university': ['name', 'phone', 'website', 'type'],
+
+    'univeristy': ['telephone']
+    
+    #'auto': ['engine', 'fuel_economy', 'model', 'price'],
+    #'book': ['author', 'isbn_13', 'publication_date', 'publisher', 'title'],
+    #'camera': ['manufacturer', 'model', 'price'],
+    #'job': ['company', 'date_posted', 'location', 'title'],
+    #'movie': ['director', 'genre', 'mpaa_rating', 'title'],
+    #'nbaplayer': ['height', 'name', 'team', 'weight'],
+    #'restaurant': ['address', 'cuisine', 'name', 'phone'],
+    #'university': ['name', 'phone', 'website', 'type'],
+    
 }
