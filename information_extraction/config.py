2266dd605d2e9f75ea6ddff28c8417ff518d7bf9
svenmeijboom <svenmeijboom@gmail.com>
2024-05-09 10:52:21 +0200

    changed data dir

--- information_extraction/config.py
+++ information_extraction/config.py
@@ -2,5 +2,6 @@ from pathlib import Path
 
 WANDB_PROJECT = 'telephone-extraction'
 #DATA_DIR = Path('~/Data/').expanduser()
-str_path = "/vol/csedu-nobackup/other/smeijboom/bachelor-thesis/Data/telephone-set/"
+#str_path = "/vol/csedu-nobackup/other/smeijboom/bachelor-thesis/Data/telephone-set/"
+str_path = "/home/smeijboom/smeijboom/bachelor-thesis/Data/telephone-set/"
 DATA_DIR = Path(str_path)
