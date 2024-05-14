400a70c27ea0f0bd2e07f06f62d044216278b4af
svenmeijboom <svenmeijboom@gmail.com>
2024-05-14 18:04:04 +0200

    fixed bug

--- information_extraction/config.py
+++ information_extraction/config.py
@@ -2,6 +2,6 @@ from pathlib import Path
 
 WANDB_PROJECT = 'MovieName-extraction'
 #DATA_DIR = Path('~/Data/').expanduser()
-str_path = "/vol/csedu-nobackup/other/smeijboom/bachelor-thesis/Data/"
+str_path = "/vol/csedu-nobackup/other/smeijboom/bachelor-thesis/Data/MovieName-set/"
 #str_path = "/home/smeijboom/smeijboom/bachelor-thesis/Data/telephone-set/"
 DATA_DIR = Path(str_path)
