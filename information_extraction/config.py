6944fafcf6e6a2ecfa3c41a333f35a866cfcc002
svenmeijboom <svenmeijboom@gmail.com>
2024-05-14 18:13:13 +0200

    adjusted for swde

--- information_extraction/config.py
+++ information_extraction/config.py
@@ -1,7 +1,8 @@
 from pathlib import Path
 
-WANDB_PROJECT = 'MovieName-extraction'
+#WANDB_PROJECT = 'MovieName-extraction'
+WANDB_PROJECT = 'swde-extraction'
 #DATA_DIR = Path('~/Data/').expanduser()
-str_path = "/vol/csedu-nobackup/other/smeijboom/bachelor-thesis/Data/MovieName-set/"
-#str_path = "/home/smeijboom/smeijboom/bachelor-thesis/Data/telephone-set/"
+#str_path = "/vol/csedu-nobackup/other/smeijboom/bachelor-thesis/Data/MovieName-set/"
+str_path = "/home/smeijboom/smeijboom/bachelor-thesis/Data/"
 DATA_DIR = Path(str_path)
