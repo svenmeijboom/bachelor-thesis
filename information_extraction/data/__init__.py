10122668ef07a7c82e291500fd0e9dff482a7179
svenmeijboom <svenmeijboom@gmail.com>
2024-05-16 00:25:51 +0200

    changed to swde movie title

--- information_extraction/data/__init__.py
+++ information_extraction/data/__init__.py
@@ -1,14 +1,17 @@
 DOMAINS = {
 
     #'Movie': ['name']
+
+    'movie': ['title']
+
     
-    'auto': ['engine', 'fuel_economy', 'model', 'price'],
-    'book': ['author', 'isbn_13', 'publication_date', 'publisher', 'title'],
-    'camera': ['manufacturer', 'model', 'price'],
-    'job': ['company', 'date_posted', 'location', 'title'],
-    'movie': ['director', 'genre', 'mpaa_rating', 'title'],
-    'nbaplayer': ['height', 'name', 'team', 'weight'],
-    'restaurant': ['address', 'cuisine', 'name', 'phone'],
-    'university': ['name', 'phone', 'website', 'type'],
+    #'auto': ['engine', 'fuel_economy', 'model', 'price'],
+    #'book': ['author', 'isbn_13', 'publication_date', 'publisher', 'title'],
+    #'camera': ['manufacturer', 'model', 'price'],
+    #'job': ['company', 'date_posted', 'location', 'title'],
+    #'movie': ['director', 'genre', 'mpaa_rating', 'title'],
+    #'nbaplayer': ['height', 'name', 'team', 'weight'],
+    #'restaurant': ['address', 'cuisine', 'name', 'phone'],
+    #'university': ['name', 'phone', 'website', 'type'],
     
 }
