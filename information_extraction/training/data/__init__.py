799065b4cdc958055005518b1492216d6bffc5c6
svenmeijboom <svenmeijboom@gmail.com>
2024-05-16 17:10:01 +0200

    .

--- information_extraction/training/data/__init__.py
+++ information_extraction/training/data/__init__.py
@@ -8,7 +8,7 @@ from .module import SWDEDataModule
 from ..trainer import MODELS
 
 
-DEFAULT_GROUND_TRUTH_DIR = Path('~/Data/swde-set/groundtruth/').expanduser()
+DEFAULT_GROUND_TRUTH_DIR = Path('~/smeijboom/bachelor-thesis/Data/swde-set/groundtruth/').expanduser()
 
 
 def get_dataset(config: wandb.Config) -> SWDEDataModule:
