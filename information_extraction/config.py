14c1cb789af9613b559397c3009026ccb0a5c655
svenmeijboom <svenmeijboom@gmail.com>
2024-06-04 16:35:18 +0200

    Changed wandb project

--- information_extraction/config.py
+++ information_extraction/config.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 
 #WANDB_PROJECT = 'MovieName-extraction'
-WANDB_PROJECT = 'MovieName-extraction'
+WANDB_PROJECT = 'bachelor-thesis'
 #DATA_DIR = Path('~/Data/').expanduser()
 #str_path = "/vol/csedu-nobackup/other/smeijboom/bachelor-thesis/Data/MovieName-set/"
 str_path = "/vol/csedu-nobackup/other/smeijboom/bachelor-thesis/Data/"
