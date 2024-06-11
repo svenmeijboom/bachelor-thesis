66624c8897a886c2dcb823b1fc83cde57c3361ca
svenmeijboom <svenmeijboom@gmail.com>
2024-06-11 08:04:53 +0200

    .

--- scripts/analysis/analyse_failures.py
+++ scripts/analysis/analyse_failures.py
@@ -19,6 +19,7 @@ def sample_failures(tables, num_failures: int = 20):
         attributes = [x[:-5] for x in df.columns if x.endswith('/true')]
 
         for attribute in attributes:
+            print(attribute)
             df_sub = df[df[f'{attribute}/em'] == 0]
             df_sub = df_sub.sample(min(num_failures, len(df_sub)))
 
