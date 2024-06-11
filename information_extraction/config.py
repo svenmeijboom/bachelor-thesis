f7c0718321dbb330ddad4ace8c695a677a25335f
svenmeijboom <svenmeijboom@gmail.com>
2024-06-11 17:46:05 +0200

    .

--- information_extraction/config.py
+++ information_extraction/config.py
@@ -1,8 +1,8 @@
 from pathlib import Path
 
 #WANDB_PROJECT = 'MovieName-extraction'
-#WANDB_PROJECT = 'swde-extraction'
-WANDB_PROJECT = 'bachelor-thesis'
+WANDB_PROJECT = 'swde-extraction'
+#WANDB_PROJECT = 'bachelor-thesis'
 #DATA_DIR = Path('~/Data/').expanduser()
 #str_path = "/vol/csedu-nobackup/other/smeijboom/bachelor-thesis/Data/MovieName-set/"
 #str_path = "/vol/csedu-nobackup/other/smeijboom/bachelor-thesis/Data2/"
@@ -11,4 +11,4 @@ DATA_DIR = Path(str_path)
 
 #DEFAULT_GROUND_TRUTH_DIR_CONF = DATA_DIR / 'swde-set200' / 'groundtruth'
 #DEFAULT_GROUND_TRUTH_DIR_CONF = DATA_DIR / 'swde-set2000' / 'groundtruth'
-DEFAULT_GROUND_TRUTH_DIR_CONF = DATA_DIR / 'SWDE' / 'groundtruth'
+DEFAULT_GROUND_TRUTH_DIR_CONF = DATA_DIR / 'swde-set200' / 'groundtruth'
